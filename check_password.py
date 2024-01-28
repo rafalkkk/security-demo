fa52fe2ad0f247b0dba669f3a4288a565eeff71a
Rafal Kraik <Rafal.Kraik@gmail.com>
2024-01-28 18:34:27 +0100

    sql-injection-check_password.py

---
+++ check_password.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+# ERROR - string manipulation for SQL query - B608
+query1 = f"SELECT * FROM users WHERE name = '{username}' AND password = '{password}'"
+
+print(f'User validation with {query1}')
+c.execute(query1)
+result = c.fetchone()
+
+if result:
+    print("Login successful")
+else:
+    print("Login failed")
+
+# ERROR - SQL string manipulation possible - B608 - silenced by "nosec"
+query2 = f"SELECT * FROM users WHERE name = ? AND password = ?"  # nosec 
+
+# B608 - still may raise errors
+print(f'User validation with {query2}')
+c.execute(query2, (username, password))
+# B608 correction by moving the code into execute command
+# c.execute("SELECT * FROM users WHERE name = ? AND password = ?", (username, password))
+
+result = c.fetchone()
+
+if result:
+    print("Login successful")
+else:
+    print("Login failed")
+
+        
